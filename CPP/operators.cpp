e7fe7a06f0bdd6081e8e67bae1ae5e4060464770
Mashrur Safir Shabab <mashrur.safir.shabab@g.bracu.ac.bd>
2023-01-02 22:03:38 +0600

    Add files via upload

--- CPP/operators.cpp
+++ CPP/operators.cpp
@@ -2,14 +2,6 @@
 using namespace std;
 
 /*
-
-----Arithmetic Operators----
-1. + (plus)
-2. - (minus)
-3. / (divide)
-4. * (multiplication)
-4. % (modulas)
-
 ----Increment/Decrement Operators----
 let a be any random variable.
 1. a++
@@ -18,16 +10,18 @@ let a be any random variable.
 4. --a
 
 */
+
 int main() {
-	int a = 1, b;
+	int a = 1;
 
 	cout << a++ << endl;
 	cout << a << endl;
-	// In line number 24, the variable a was used to print out its output and then it was incremented by one.
-	// In line number 25, the value of a was simply printed.
+	// In line number 24, the variable "a" was used to print out its output and then it was incremented by value one.
+	// In line number 25, the value of "a" was simply printed.
 
 	cout << ++a << endl;
-	// In line 29, firstly "a" was incremented by one and then it was printed.
+	// In line 29, firstly "a" was incremented by value one and then it was printed.
+	// This whole mechanism applies verily for "a--" and "--a".
 
 	return 0;
 }
