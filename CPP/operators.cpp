d6fe359ad78e06ce7ac056811d9894fd1ffa6bc0
Mashrur Safir Shabab <mashrur.safir.shabab@g.bracu.ac.bd>
2023-01-02 22:01:10 +0600

    Add files via upload

---
+++ CPP/operators.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+
+----Arithmetic Operators----
+1. + (plus)
+2. - (minus)
+3. / (divide)
+4. * (multiplication)
+4. % (modulas)
+
+----Increment/Decrement Operators----
+let a be any random variable.
+1. a++
+2. a--
+3. ++a
+4. --a
+
+*/
+int main() {
+	int a = 1, b;
+
+	cout << a++ << endl;
+	cout << a << endl;
+	// In line number 24, the variable a was used to print out its output and then it was incremented by one.
+	// In line number 25, the value of a was simply printed.
+
+	cout << ++a << endl;
+	// In line 29, firstly "a" was incremented by one and then it was printed.
+
+	return 0;
+}
+
