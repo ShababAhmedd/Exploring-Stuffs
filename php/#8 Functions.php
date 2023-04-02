d893efedb3d58796d2caa8b9584acea3ac8a3862
Mashrur Safir Shabab <mashrur.safir.shabab@g.bracu.ac.bd>
2023-04-03 00:43:54 +0600

    Create #8 Functions.php

---
+++ php/#8 Functions.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Functions</title>
+</head>
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.container{
+
+    max-width: 80%;
+    background-color: grey; 
+    margin: auto;
+    padding: 23px;
+}
+</style>
+<body>
+    <div class="container">
+        This is a container.
+        <h1> Functions </h1>
+        <p> </p>
+        <p> <p>
+    
+
+    <?php
+        function print5(){      // In php, writing a function starts with the name "Function" itself.
+            echo "Five";
+        }
+
+    print5()
+    ?>    
+    
+    </div>
+</body>
+</html>
