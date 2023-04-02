791da98396e6e6b7b525df5bfa7eecbb8f49813c
Mashrur Safir Shabab <mashrur.safir.shabab@g.bracu.ac.bd>
2023-04-02 18:02:19 +0600

    Create comments.php

---
+++ php/comments.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning PHP - Comments</title>
+</head>
+<body>
+    <div class="container">
+        This is my  first php website
+    </div>
+
+    <?php
+    /*
+    This is the starting 
+    and 
+    ending 
+    block to write php codes.      
+    */
+    
+    // Meanwhile single line comment is simply written with a double slash while multile like comments is written the way demonstrated on line 16-19
+    ?>    
+    
+
+</body>
+</html>
