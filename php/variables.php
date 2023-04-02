4380ee6dba87727501291da89ab9b9038eb710ec
Mashrur Safir Shabab <mashrur.safir.shabab@g.bracu.ac.bd>
2023-04-02 18:05:59 +0600

    Create variables.php

---
+++ php/variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning PHP - Variables</title>
+</head>
+<body>
+    <div class="container">
+        This is my  first php website
+    </div>
+
+    <?php
+        $variable1 = 99;
+        echo $variable1;
+        // in php the name of the variable always starts with the sign " $ "
+        // echo is basically the print function.
+    ?>    
+    
+
+</body>
+</html>
